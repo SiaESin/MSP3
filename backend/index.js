ce03f833d5d04ddc7a58581c96223ebd7ddbb99a
SiaESin <siabhon.sindab@gmail.com>
2023-10-21 14:20:24 -0700

    backend index,js and controllers

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
 const express = require('express')
+
 const app = express()
 
 app.get('/', (req, res) => {
     res.send('Books Rule!')
 })
 
+app.get('*', (req, res) => {
+    res.status(404).send('Not Found')
+})
 app.listen(process.env.PORT)
