39931f8320e7da87754addada13e3dd2af0aef7f
SiaESin <siabhon.sindab@gmail.com>
2023-10-23 20:50:42 -0700

    node module corrections and debugging

--- client/src/Components/books.js
+++ client/src/Components/books.js
@@ -1,45 +1,45 @@
-import { useEffect, useState } from "react";
-import { useHistory } from "react-router";
+// import { useEffect, useState } from "react";
+// // import { useHistory } from "react-router";
 
-export default function Books() {
+// export default function Books() {
 
-	const history = useHistory()
+// 	const history = useHistory()
 	
-	const [books, setBooks] = useState([])
+// 	const [books, setBooks] = useState([])
 
-	useEffect(() => {
-		const fetchData = async () => {
-			const response = await fetch(`http://localhost:3000/books`)
-			const resData = await response.json()
-			setBooks(resData)
-		}
-		fetchData()
-	}, [])
+// 	useEffect(() => {
+// 		const fetchData = async () => {
+// 			const response = await fetch(`http://localhost:3000/books`)
+// 			const resData = await response.json()
+// 			setBooks(resData)
+// 		}
+// 		fetchData()
+// 	}, [])
 
-	let booksFormatted = books.map((books) => {
-		return (
-			<div className="col-sm-6" key={books.id}>
-				<h2>
-					<a href="#" onClick={() => history.push(`/books/${books.id}`)} >
-						{books.name}
-					</a>
-				</h2>
-				<p className="text-center">
-					{books.description}
-				</p>
-				<img style={{ maxWidth: 200 }} src={books.pic} alt={books.name} />
-				<p className="text-center">
-					{books.price}
-				</p>
-			</div>
-		)
-	})
-	return (
-		<main>
-			<h1>See our Amazing selection of Books!</h1>
-			<div className="row">
-				{booksFormatted}
-			</div>
-		</main>
-	)
-}
+// 	let booksFormatted = books.map((books) => {
+// 		return (
+// 			<div className="col-sm-6" key={books.id}>
+// 				<h2>
+// 					<a href="#" onClick={() => history.push(`/books/${books.id}`)} >
+// 						{books.name}
+// 					</a>
+// 				</h2>
+// 				<p className="text-center">
+// 					{books.description}
+// 				</p>
+// 				<img style={{ maxWidth: 200 }} src={books.pic} alt={books.name} />
+// 				<p className="text-center">
+// 					{books.price}
+// 				</p>
+// 			</div>
+// 		)
+// 	})
+// 	return (
+// 		<main>
+// 			<h1>See our Amazing selection of Books!</h1>
+// 			<div className="row">
+// 				{booksFormatted}
+// 			</div>
+// 		</main>
+// 	)
+// }
