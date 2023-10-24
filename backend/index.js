2688590e1dd2a5cc5ac9b9ee399f3626033408b4
SiaESin <siabhon.sindab@gmail.com>
2023-10-24 14:29:44 -0700

    seeding books & backend changes

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const cors = require('cors')
+const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const app = express()
 
-app.use(cors())
+app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
+app.use(methodOverride('_method'))
 app.use('/', require('./controllers/bookstore'))
 app.use('/', require('./controllers/users'))
 
