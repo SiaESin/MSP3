f5e358a5b3c66b17a424145b3386032baeae774b
Nalinaka <nalinaka@gmail.com>
2023-10-25 19:53:42 -0700

    Fixed Nav bar from Aly

--- client/src/Navigation.js
+++ client/src/Navigation.js
@@ -1,3 +1,19 @@
+export default function Nav() {
+
+    return (
+        <nav className="flex-container">
+            <div className="nav-link"><a href="/">Book Boulevard</a></div>
+            <div className="nav-link"><a href="/about">About</a></div>
+            <div className="nav-link"><a href="/books">Books</a></div>
+            <div className="nav-link"><a href="/contact">Contact</a></div>
+            <div className="nav-link"><a href="/login">Login</a></div>
+            <div className="nav-link"><a href="/signup">Sign Up</a></div>
+        </nav>
+    )
+}
+
+
+
 // import { useState, useEffect, useContext } from 'react'
 // import { useHistory } from "react-router";
 // import { CurrentUser } from '.client/src/contexts/CurrentUser.js';
@@ -20,22 +36,22 @@
 //     });
 // }
         
-export default function Nav() {
+// export default function Nav() {
 
-    return (
-        <nav>
-            <ul>
-                <li><a href="/">Book Boulevard</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/books">Books</a></li>
-                {/* <li><a href="/bookdetails">Book Details</a></li> */}
-                <li><a href="/contact">Contact</a></li>
-                <li><a href="/login">Login</a></li>
-                <li><a href="/signup">Sign Up</a></li>
-            </ul>
-        </nav>
-    )
-}
+//     return (
+//         <nav>
+//             <ul>
+//                 <li><a href="/">Book Boulevard</a></li>
+//                 <li><a href="/about">About</a></li>
+//                 <li><a href="/books">Books</a></li>
+//                 {/* <li><a href="/bookdetails">Book Details</a></li> */}
+//                 <li><a href="/contact">Contact</a></li>
+//                 <li><a href="/login">Login</a></li>
+//                 <li><a href="/signup">Sign Up</a></li>
+//             </ul>
+//         </nav>
+//     )
+// }
 
 // export default Navigation; 
 
