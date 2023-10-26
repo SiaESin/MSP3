efb5175b68a65ab5b40df9570c1768423f6f1393
SiaESin <siabhon.sindab@gmail.com>
2023-10-26 01:03:31 -0700

    adding user registration

--- backend/index.js
+++ backend/index.js
@@ -33,31 +33,3 @@ app.listen(PORT, () => {
 });
 
 
-// const express = require('express');
-// const cors = require('cors');
-// const mongoose = require('mongoose');
-// const app = express();
-
-// app.use(cors());
-// app.use(express.static('public'));
-// app.use(express.urlencoded({extended: true}));
-
-// // Routes
-// app.use('/', require('./controllers/bookstore'));
-// app.use('/', require('./controllers/users'));
-
-// app.get('/', (req, res) => {
-//     res.render('home')
-// })
-
-// require('dotenv').config()
-// const PORT = process.env.PORT || 3000
-// mongoose.connect("process.env.MONGO_URI", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true},
-//     () => {
-//         console.log('connected to MongoDB')
-//     }   
-// )
-
-// app.listen(process.env.PORT, () => console.log('Server running!'))
