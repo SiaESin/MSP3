3217f71b48e2b0d4e31d36c4c54aef79131b2820
alyStrom5062 <Alyenastrom00@gmail.com>
2023-10-26 16:25:23 -0700

    Updated css both mobile and desktop

--- client/src/Components/contact.js
+++ client/src/Components/contact.js
@@ -7,12 +7,13 @@ export default function Contact() {
 		<main>
 			<h1>Contact Us</h1>
 			<div className="contact">
-				<p>Thank you for visiting our website!</p>
-				<p>Feel free to contact us at any time!</p>
-
-				<p>Address: 123 Main St, Anytown, USA</p>
-				<p>Phone: (888) 777-4321</p>
-				<p>Email: <a href="mailto:enquiries@bookboulevard.com">enquiries@bookboulevard.com</a></p>
+				<h2>Thank you for visiting our website!</h2>
+				<h3>Feel free to contact us at any time!</h3>
+				<div className="contact-info">
+					<p>Address: <b>123 Main St, Anytown, USA</b></p>
+					<p>Phone: <b>(888) 777-4321</b></p>
+					<p>Email: <a href="mailto:enquiries@bookboulevard.com">enquiries@bookboulevard.com</a></p>
+				</div>
 			</div>
 		</main>
 	)
