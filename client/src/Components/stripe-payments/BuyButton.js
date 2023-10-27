9ffe5f8ca229006f8035bb088e73676d50275144
alyStrom5062 <Alyenastrom00@gmail.com>
2023-10-27 16:11:51 -0700

    Stripe button array connected

--- client/src/Components/stripe-payments/BuyButton.js
+++ client/src/Components/stripe-payments/BuyButton.js
@@ -1,63 +1,83 @@
 import * as React from 'react';
+import { productLinks } from '../bookData';
+import { useParams } from 'react-router';
 
 function BuyButtonComponent() {
+
+  const { index } = useParams();
+	const selectedBook = parseInt(index);
+	const paymentLink = productLinks[selectedBook]
+
   // Paste the stripe-buy-button snippet in your React component
   return (
-    <stripe-buy-button
+  <div>
+  <stripe-buy-button
+    buy-button-id={paymentLink}
+    publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy">
+  </stripe-buy-button>
+
+{/* The Inheritance Games  */}
+<stripe-buy-button
   buy-button-id="buy_btn_1O5e8iJNUdFsy0RxgeJcQcq1"
-  publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
-   >
-    </stripe-buy-button>
-)};
-{
+  publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy">
+</stripe-buy-button>
+
+{/* Hawthorne Legacy */}
 <stripe-buy-button
   buy-button-id="buy_btn_1O5e7UJNUdFsy0RxdMTRQVu7"
-  publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
->
+  publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy">
 </stripe-buy-button>
-  };
-  {
-<stripe-buy-button
-  buy-button-id="buy_btn_1O5e4PJNUdFsy0RxZKL3gNgR"
-  publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
->
-</stripe-buy-button>
-  };
-{
-<stripe-buy-button
-  buy-button-id="buy_btn_1O5e06JNUdFsy0RxfhpJusRR"
-  publishable-key="pk_live_51O4tNdJNUdFsy0RxFlyGv5xfD32NdCxk2gmAQtl9bwWMTc1uf8oDMEisucnVJ4OosRWzcEb6FDaIBgFQztZ6JkH800vix2hlXo"
->
-</stripe-buy-button>
-};
-{
+
+{/* The Final Gambit  */}
 <stripe-buy-button
   buy-button-id="buy_btn_1O5djjJNUdFsy0RxwIictUzY"
   publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
 >
 </stripe-buy-button>
-};
-{
-<stripe-buy-button
-  buy-button-id="buy_btn_1O5dxqJNUdFsy0RxsCEGDLOo"
-  publishable-key="pk_live_51O4tNdJNUdFsy0RxFlyGv5xfD32NdCxk2gmAQtl9bwWMTc1uf8oDMEisucnVJ4OosRWzcEb6FDaIBgFQztZ6JkH800vix2hlXo"
->
-</stripe-buy-button>
-};
-{
+
+{/* The Brothers Hawthorne  */}
 <stripe-buy-button
   buy-button-id="buy_btn_1O5duPJNUdFsy0RxcId6BeLr"
   publishable-key="pk_live_51O4tNdJNUdFsy0RxFlyGv5xfD32NdCxk2gmAQtl9bwWMTc1uf8oDMEisucnVJ4OosRWzcEb6FDaIBgFQztZ6JkH800vix2hlXo"
 >
 </stripe-buy-button>
-};
-{
+
+{/* Angels and Demons  */}
 <stripe-buy-button
-  buy-button-id="buy_btn_1O5dk3JNUdFsy0RxrDoo9WIf"
+  buy-button-id="buy_btn_1O5e06JNUdFsy0RxfhpJusRR"
+  publishable-key="pk_live_51O4tNdJNUdFsy0RxFlyGv5xfD32NdCxk2gmAQtl9bwWMTc1uf8oDMEisucnVJ4OosRWzcEb6FDaIBgFQztZ6JkH800vix2hlXo"
+>
+</stripe-buy-button>
+
+{/* Moon Called */}
+<stripe-buy-button
+  buy-button-id="buy_btn_1O5e4PJNUdFsy0RxZKL3gNgR"
   publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
 >
 </stripe-buy-button>
+
+{/* Perks of Being a Wallflower  */}
+<stripe-buy-button
+  buy-button-id="buy_btn_1O5dk3JNUdFsy0RxrDoo9WIf"
+  publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
+  >
+</stripe-buy-button>
+
+
+{/* Shelter  */}
+<stripe-buy-button
+  buy-button-id="buy_btn_1O5dxqJNUdFsy0RxsCEGDLOo"
+  publishable-key="pk_live_51O4tNdJNUdFsy0RxFlyGv5xfD32NdCxk2gmAQtl9bwWMTc1uf8oDMEisucnVJ4OosRWzcEb6FDaIBgFQztZ6JkH800vix2hlXo"
+>
+</stripe-buy-button>
+
+
+  </div>
+  )
 };
+
+
+
 
 export default BuyButtonComponent;
 
