f15f026478524232d14ac5b0f54341e3a49f0022
Nalinaka <nalinaka@gmail.com>
2023-10-27 16:20:29 -0700

    completed stripe changes

--- client/src/Components/stripe-payments/BuyButton.js
+++ client/src/Components/stripe-payments/BuyButton.js
@@ -1,65 +1,65 @@
-import * as React from 'react';
+// import * as React from 'react';
 
-function BuyButtonComponent() {
-  // Paste the stripe-buy-button snippet in your React component
-  return (
-    <stripe-buy-button
-  buy-button-id="buy_btn_1O5e8iJNUdFsy0RxgeJcQcq1"
-  publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
-   >
-    </stripe-buy-button>
-)};
-{
-<stripe-buy-button
-  buy-button-id="buy_btn_1O5e7UJNUdFsy0RxdMTRQVu7"
-  publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
->
-</stripe-buy-button>
-  };
-  {
-<stripe-buy-button
-  buy-button-id="buy_btn_1O5e4PJNUdFsy0RxZKL3gNgR"
-  publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
->
-</stripe-buy-button>
-  };
-{
-<stripe-buy-button
-  buy-button-id="buy_btn_1O5e06JNUdFsy0RxfhpJusRR"
-  publishable-key="pk_live_51O4tNdJNUdFsy0RxFlyGv5xfD32NdCxk2gmAQtl9bwWMTc1uf8oDMEisucnVJ4OosRWzcEb6FDaIBgFQztZ6JkH800vix2hlXo"
->
-</stripe-buy-button>
-};
-{
-<stripe-buy-button
-  buy-button-id="buy_btn_1O5djjJNUdFsy0RxwIictUzY"
-  publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
->
-</stripe-buy-button>
-};
-{
-<stripe-buy-button
-  buy-button-id="buy_btn_1O5dxqJNUdFsy0RxsCEGDLOo"
-  publishable-key="pk_live_51O4tNdJNUdFsy0RxFlyGv5xfD32NdCxk2gmAQtl9bwWMTc1uf8oDMEisucnVJ4OosRWzcEb6FDaIBgFQztZ6JkH800vix2hlXo"
->
-</stripe-buy-button>
-};
-{
-<stripe-buy-button
-  buy-button-id="buy_btn_1O5duPJNUdFsy0RxcId6BeLr"
-  publishable-key="pk_live_51O4tNdJNUdFsy0RxFlyGv5xfD32NdCxk2gmAQtl9bwWMTc1uf8oDMEisucnVJ4OosRWzcEb6FDaIBgFQztZ6JkH800vix2hlXo"
->
-</stripe-buy-button>
-};
-{
-<stripe-buy-button
-  buy-button-id="buy_btn_1O5dk3JNUdFsy0RxrDoo9WIf"
-  publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
->
-</stripe-buy-button>
-};
+// function BuyButtonComponent() {
+//   // Paste the stripe-buy-button snippet in your React component
+//   return (
+//     <stripe-buy-button
+//   buy-button-id="buy_btn_1O5e8iJNUdFsy0RxgeJcQcq1"
+//   publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
+//    >
+//     </stripe-buy-button>
+// )};
+// {
+// <stripe-buy-button
+//   buy-button-id="buy_btn_1O5e7UJNUdFsy0RxdMTRQVu7"
+//   publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
+// >
+// </stripe-buy-button>
+//   };
+//   {
+// <stripe-buy-button
+//   buy-button-id="buy_btn_1O5e4PJNUdFsy0RxZKL3gNgR"
+//   publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
+// >
+// </stripe-buy-button>
+//   };
+// {
+// <stripe-buy-button
+//   buy-button-id="buy_btn_1O5e06JNUdFsy0RxfhpJusRR"
+//   publishable-key="pk_live_51O4tNdJNUdFsy0RxFlyGv5xfD32NdCxk2gmAQtl9bwWMTc1uf8oDMEisucnVJ4OosRWzcEb6FDaIBgFQztZ6JkH800vix2hlXo"
+// >
+// </stripe-buy-button>
+// };
+// {
+// <stripe-buy-button
+//   buy-button-id="buy_btn_1O5djjJNUdFsy0RxwIictUzY"
+//   publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
+// >
+// </stripe-buy-button>
+// };
+// {
+// <stripe-buy-button
+//   buy-button-id="buy_btn_1O5dxqJNUdFsy0RxsCEGDLOo"
+//   publishable-key="pk_live_51O4tNdJNUdFsy0RxFlyGv5xfD32NdCxk2gmAQtl9bwWMTc1uf8oDMEisucnVJ4OosRWzcEb6FDaIBgFQztZ6JkH800vix2hlXo"
+// >
+// </stripe-buy-button>
+// };
+// {
+// <stripe-buy-button
+//   buy-button-id="buy_btn_1O5duPJNUdFsy0RxcId6BeLr"
+//   publishable-key="pk_live_51O4tNdJNUdFsy0RxFlyGv5xfD32NdCxk2gmAQtl9bwWMTc1uf8oDMEisucnVJ4OosRWzcEb6FDaIBgFQztZ6JkH800vix2hlXo"
+// >
+// </stripe-buy-button>
+// };
+// {
+// <stripe-buy-button
+//   buy-button-id="buy_btn_1O5dk3JNUdFsy0RxrDoo9WIf"
+//   publishable-key="pk_test_51O4tNdJNUdFsy0RxW2OOoCizqLfojhZ2hz23uR8n7ARHHoFMwBTevbQCB7AAzn2gj5Ky8Cr1REZQoqUmv05m86PF00xW4cGpzy"
+// >
+// </stripe-buy-button>
+// };
 
-export default BuyButtonComponent;
+// export default BuyButtonComponent;
 
 
 // The Inheritance Games:
